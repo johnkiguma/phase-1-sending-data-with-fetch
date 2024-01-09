82bdac0a457350973104810788f74c90be17c926
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-09 09:29:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+          name: userName,
+          email: userEmail
+      })
+  })
+      .then(response => response.json())
+      .then(function (object) {
+          document.body.innerHTML = object['id']
+
+      })
+      .catch(function (error) {
+          document.body.innerHTML = error.message
+      })
+}
